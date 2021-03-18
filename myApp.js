ea574eddbf184965541f1f514ed68f54de93bff3
Joseph Olabisi <josepholabisi6000@gmail.com>
2021-03-18 12:01:05 +0100

    Mongo setup

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
+let mongoose = require("mongoose");
 
+try {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} catch (e) {
+  console.warn(e);
+}
 
 let Person;
 
