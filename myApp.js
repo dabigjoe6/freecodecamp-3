48e54c9806f368a5fe4b57be688484267d002de0
Joseph Olabisi <josepholabisi6000@gmail.com>
2021-03-18 17:09:12 +0100

    Query chain

--- myApp.js
+++ myApp.js
@@ -150,7 +150,13 @@ const removeManyPeople = async (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: "asc" })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((err, data) => {
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
