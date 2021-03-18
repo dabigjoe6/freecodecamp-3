0d82a42546340cb738c1326c74160b95e6f77870
Joseph Olabisi <josepholabisi6000@gmail.com>
2021-03-18 15:16:43 +0100

    possible fix

--- myApp.js
+++ myApp.js
@@ -2,10 +2,7 @@ require("dotenv").config();
 let mongoose = require("mongoose");
 
 try {
-  mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(process.env.MONGO_URI);
 } catch (e) {
   console.warn(e);
 }
